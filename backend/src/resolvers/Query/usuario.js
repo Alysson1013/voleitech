a97efccd8f7f75c470613de7e0d9bfe00a32921c
Alysson <alysson6801@hotmail.com>
2021-05-02 16:26:46 -0300

    feat: usuario Query resolver

---
+++ backend/src/resolvers/Query/usuario.js
@@ -0,0 +1,38 @@
+const db = require('../../config/db')
+const bcrypt = require('bcrypt-nodejs')
+const { getUserLogged } = require('../UsuarioLogado')
+
+module.exports = {
+    async login(_, { dados }) {
+        const usuario = await db('usuario')
+            .where({ email: dados.email })
+            .first()
+
+        if (!usuario) throw new Error('Usuario/Senha Inválido')
+
+        const equal = bcrypt.compareSync(dados.senha, usuario.senha)
+        if (!equal) throw new Error('Usuario/Senha Inválido')
+
+        return getUserLogged(usuario)
+    },
+    usuarios(_, args, ctx) {
+        ctx && ctx.validarAdmin()
+        return db('usuarios')
+    },
+    usuario(_, { filtros }, ctx) {
+        ctx && ctx.validarUsuarioFiltro()
+        if (!filtro) return null
+        const { id, email } = filtro
+        if (id) {
+            return db('usuarios')
+                .where({ id })
+                .first()
+        } else if (email) {
+            return db('usuarios')
+                .where({ email })
+                .first()
+        } else {
+            return null
+        }
+    }
+}
---
+++ backend/src/resolvers/UsuarioLogado.js
@@ -0,0 +1,23 @@
+const jwt = require('jwt-simple')
+
+module.exports = {
+    async getUserLogged(usuario){
+        const now = Math.floor(Date.now() / 1000)
+
+        const userInfo = {
+            id: usuario.id,
+            nome: usuario.nome,
+            email: usuario.email,
+            papel: usuario.papel,
+            iat: now,
+            exp: now + (3 * 24 * 60 * 60)
+        }
+
+        const authSecret = process.env.APP_AUTH_SECRET
+
+        return {
+            ...usuarioInfo,
+            token: jwt.encode(usuarioInfo, authSecret)
+        }
+    }
+}
