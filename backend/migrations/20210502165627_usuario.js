f9521718409fde2c4f4bee632ca7c5aab61dd13d
Alysson <alysson6801@hotmail.com>
2021-05-02 14:53:25 -0300

    chore: Graphql schema of Usuario

--- backend/migrations/20210502165627_usuario.js
+++ backend/migrations/20210502165627_usuario.js
@@ -2,13 +2,14 @@
 exports.up = function (knex) {
     return knex.schema.createTable('usuario', table => {
         table.increments('id').primary()
-        table.string('name', 50).notNull()
+        table.string('nome', 50).notNull()
         table.string('email', 80).notNull().unique()
-        table.date('dt_birth').notNull()
-        table.string('n_subscription_t').notNull().unique()
-        table.string('description', 500)
-        table.string('password', 500).notNull()
-        table.timestamp('dt_creation').defaultTo(knex.fn.now())
+        table.date('dt_nasc').notNull()
+        table.string('n_inscricao_t').notNull().unique()
+        table.string('descricao', 500)
+        table.string('senha', 500).notNull()
+        table.integer('papel').defaultTo(0)
+        table.timestamp('dt_criacao').defaultTo(knex.fn.now())
     })
 };
 
