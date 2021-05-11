b692b32a5095dacc34e00d3ea24ae11467343dce
Alysson <alysson6801@hotmail.com>
2021-05-11 20:54:35 -0300

    add: collaborators model

---
+++ backend/migrations/20210511224715_collaborator.js
@@ -0,0 +1,34 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('collaborators', table => {
+        table.increments('id').primary()
+        table.integer('user_id').unsigned()
+        table.string('phone_1', 12).notNull().unique()
+        table.string('phone_2', 12).unique()
+        table.string('phone_3', 12).unique()
+        table.string('email_1', 30).notNull().unique()
+        table.string('email_2', 30).unique()
+        table.string('email_3', 30).unique()
+        table.string('name', 150).notNull()
+        table.datetime('dt_birth').notNull()
+        table.enu('function', ['athlete', 'assistant', 'both'])
+        table.string('n_enrollment_atl')
+        table.string('n_enrollment_ast')
+        table.string('positions', 150)
+        table.integer('n_uniform')
+        table.float('height')
+        table.float('Weight')
+        table.float('width')
+        table.enu('gender', ['male', 'female'])
+        table.float('bmi')
+        table.float('jump_distance')
+        table.float('jump_height')
+        table.string('describe', 500)
+
+        table.foreign('user_id').references('users.id')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('collaborators')
+};
