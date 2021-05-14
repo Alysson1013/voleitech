aa75351413aaf0cfa755f6fab98f82782e0efa41
Alysson <alysson6801@hotmail.com>
2021-05-14 11:30:57 -0300

    add: athletes training results  model

---
+++ backend/migrations/20210514141950_athletes_training_results.js
@@ -0,0 +1,18 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('athletes_training_results', table => {
+        table.increments('id').primary()
+        table.integer('collaborator_athlete_id').unsigned().notNull()
+        table.integer('training_id').unsigned().notNull()
+        table.integer('result_id').unsigned().notNull()
+        table.boolean('status').defaultTo(true)
+
+        table.foreign('collaborator_athlete_id').references('collaborators.id')
+        table.foreign('training_id').references('training.id')
+        table.foreign('result_id').references('results.id')
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('athletes_training_results')
+};
