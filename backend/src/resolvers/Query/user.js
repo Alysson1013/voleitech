4ceb278a6911e1286a5cdc8f9bd704dc246c5af4
Alysson <alysson6801@hotmail.com>
2021-05-16 15:47:03 -0300

    chore: login resolver

--- backend/src/resolvers/Query/user.js
+++ backend/src/resolvers/Query/user.js
@@ -1,7 +1,26 @@
 const db = require("../../../config/db")
+const bcrypt = require("bcrypt-nodejs")
+const { getUserLogged } = require('../Common/User')
 
 module.exports = {
-    async users (_, args){
+    async login(_, { data }) {
+        const user = await db('users')
+            .where({ email: data.email })
+            .first()
+
+        if (!user) {
+            throw new Error('User/Password Invalid')
+        }
+
+        const same = bcrypt.compareSync(data.password, user.password)
+
+        if (!same) {
+            throw new Error('User/Password Invalid')
+        }
+
+        return getUserLogged(user)
+    },
+    async users(_, args) {
         return db('users')
     }
 }
