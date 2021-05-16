6b5f1483588eb991a445a8efd569f028d95da981
Alysson <alysson6801@hotmail.com>
2021-05-16 15:59:35 -0300

    chore: get user resolver

--- backend/src/resolvers/Query/user.js
+++ backend/src/resolvers/Query/user.js
@@ -22,5 +22,21 @@ module.exports = {
     },
     async users(_, args) {
         return db('users')
+    },
+    async user(_, { filter }) {
+        if (!filter) return null
+        const { id, email } = filter
+
+        if (id) {
+            return db('users')
+            .where({ id })
+            .first()
+        } else if (email) {
+            return db('users')
+            .where({ email })
+            .first()
+        } else {
+            return null
+        }
     }
 }
