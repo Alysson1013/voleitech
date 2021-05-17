70e87ff6ed954fc98f660779fcdc1721519dcc89
Alysson <alysson6801@hotmail.com>
2021-05-17 17:20:37 -0300

    chore: add context object

---
+++ backend/src/context.js
@@ -0,0 +1,36 @@
+const jwt = require('jwt-simple')
+
+module.exports = async ({ req }) => {
+    const auth = req.headers.authorization
+    const token = auth && auth.substring(7)
+
+    let user = null
+
+    if (token) {
+        try {
+            let tokenContent = jwt.decode(token, process.env.APP_AUTH_SECRET)
+            if (new Date(tokenContent.exp * 1000) > new Date()) {
+                user = tokenContent
+            }
+        } catch (error) {
+
+        }
+    }
+
+    const err = "Access Denied"
+
+    return {
+        user,
+        userValidate() {
+            if (!user) throw err
+        },
+        userValidateFilter(filter) {
+            if (!user) throw err
+            if (!filter) throw err
+
+            const { id } = filter
+            if (!id) throw err
+            if (id && id !== user.id) throw err
+        }
+    }
+}
