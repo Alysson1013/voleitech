fadadf7c437d8fc98fe0b46dae047389d4a1ee98
Alysson <alysson6801@hotmail.com>
2021-05-18 11:46:44 -0300

    chore: add adress resolver

---
+++ backend/src/resolvers/Mutation/address.js
@@ -0,0 +1,17 @@
+const db = require('../../../config/db')
+
+const mutations = {
+    async newAddress(_, { data }) {
+        try {
+            const [id] = await db('adresses')
+                .insert(data)
+            return db('adresses')
+                .where({ id })
+                .first()
+        } catch (error) {
+            throw new Error(error.sqlMessage)
+        }
+    }
+}
+
+module.exports = mutations
