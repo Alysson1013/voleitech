cf271de7ab7421a22f0f1d2f749d048a0a6f7fd7
Alysson <alysson6801@hotmail.com>
2021-05-18 21:26:15 -0300

    chore: add category resolver

--- backend/src/resolvers/Query/category.js
+++ backend/src/resolvers/Query/category.js
@@ -3,5 +3,23 @@ const db = require('../../../config/db')
 module.exports = {
     async categories(_, args){
         return db('team_category')
+    },
+    async category(_, { filter }){
+        if (!filter) return null
+
+        const { id, name_category, user_id } = filter
+
+        if (id){
+            return db('team_category')
+                .where({ id })
+        } else if (name_category) {
+            return db('team_category')
+                .where({ name_category })
+        } else if (user_id) {
+            return db('team_category')
+                .where({ user_id })
+        } else {
+            return null
+        }
     }
 }
