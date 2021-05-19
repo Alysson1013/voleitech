c56efecf43cbe1f75e721374eb6e5421555dfa52
Alysson <alysson6801@hotmail.com>
2021-05-19 11:51:39 -0300

    chore: get team by id and name resolver

--- backend/src/resolvers/Query/team.js
+++ backend/src/resolvers/Query/team.js
@@ -1,10 +1,33 @@
 const db = require('../../../config/db')
 
 module.exports = {
-    async teams(_, args){
+    async teams(_, args) {
         return db
-            .select(["teams.id as id", "teams.name as name","team_category.name_category as category", "average_result", "average_age", "average_height", "average_weight", "teams.describe as describe", "user_id", "gender"])
+            .select(["teams.id as id", "teams.name as name", "team_category.name_category as category", "average_result", "average_age", "average_height", "average_weight", "teams.describe as describe", "user_id", "gender"])
             .table("teams")
             .innerJoin("team_category", "teams.category_id", "team_category.id")
+    },
+    async team(_, { filter }) {
+        if (!filter) return null
+
+        const { id, name } = filter
+
+        if (id) {
+            return db
+                .select(["teams.id as id", "teams.name as name", "team_category.name_category as category", "average_result", "average_age", "average_height", "average_weight", "teams.describe as describe", "user_id", "gender"])
+                .table("teams")
+                .innerJoin("team_category", "teams.category_id", "team_category.id")
+                .whereRaw(`teams.id = ${id}`)
+                .first()
+        } else if (name) {
+            return db
+                .select(["teams.id as id", "teams.name as name", "team_category.name_category as category", "average_result", "average_age", "average_height", "average_weight", "teams.describe as describe", "user_id", "gender"])
+                .table("teams")
+                .innerJoin("team_category", "teams.category_id", "team_category.id")
+                .where({name})
+                .first()
+        } else {
+            return null
+        }
     }
 }
