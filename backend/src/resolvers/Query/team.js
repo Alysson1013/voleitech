f2beff03c3809cecb6ae8b36c29808cf1412293a
Alysson <alysson6801@hotmail.com>
2021-05-19 15:52:02 -0300

    chore: get teams in collaborator schema

--- backend/src/resolvers/Query/team.js
+++ backend/src/resolvers/Query/team.js
@@ -3,7 +3,7 @@ const db = require('../../../config/db')
 module.exports = {
     async teams(_, args) {
         return db
-            .select(["teams.id as id", "teams.name as name", "team_category.name_category as category", "team_category.id as category_id", "average_result", "average_age", "average_height", "average_weight", "teams.describe as describe", "user_id", "gender"])
+            .select(["teams.id as id", "teams.name as name", "team_category.id as category_id", "team_category.name_category as category", "average_result", "average_age", "average_height", "average_weight", "teams.describe as describe", "user_id", "gender"])
             .table("teams")
             .innerJoin("team_category", "teams.category_id", "team_category.id")
     },
