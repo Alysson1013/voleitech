1fbe7de5e6034ab49de02a5ba409c0953f221b07
Alysson <alysson6801@hotmail.com>
2021-05-21 17:03:53 -0300

    chore: input of collaborators teams in collaboration mutation

--- backend/src/resolvers/Mutation/collaborator.js
+++ backend/src/resolvers/Mutation/collaborator.js
@@ -5,8 +5,19 @@ const mutations = {
         ctx && ctx.userValidate()
         try {
             data.user_id = ctx.user.id
+            const teams = data.teams 
+            delete data.teams
+
             const [id] = await db('collaborators')
                 .insert(data)
+
+            for (let team of teams) {
+                team.collaborator_id = id
+                console.log(team)
+                await db('collaborators_teams')
+                    .insert(team)
+            }
+
             return db('collaborators')
                 .where({ id })
                 .first()
