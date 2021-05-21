cf1efb1c2123cf3f84f69041209f839d75018df3
Alysson <alysson6801@hotmail.com>
2021-05-21 15:17:01 -0300

    chore: get by id collaborator team

--- backend/src/resolvers/Query/collaborator_team.js
+++ backend/src/resolvers/Query/collaborator_team.js
@@ -1,9 +1,21 @@
 const db = require('../../../config/db')
 
 module.exports = {
-    async collaborators_teams(_, args){
+    async collaborators_teams(_, args) {
         return db
             .select()
             .table('collaborators_teams')
+    },
+    async collaborator_team(_, { filter }) {
+        if (!filter) return null
+
+        const { id } = filter
+        if (id) {
+            return db('collaborators_teams')
+                .where({ id })
+                .first()
+        } else {
+            return null
+        }
     }
 }
