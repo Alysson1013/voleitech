bed45952f228515d1ce8e2b929cce2b11268a592
Alysson <alysson6801@hotmail.com>
2021-05-26 20:48:09 -0300

    chore: get athlete_training_result by id

---
+++ backend/src/resolvers/Mutation/athlete_training_result.js
@@ -0,0 +1,7 @@
+const db = require('../../../config/db')
+
+const mutations = {
+    async 
+}
+
+module.exports = mutations
--- backend/src/resolvers/Query/athlete_training_result.js
+++ backend/src/resolvers/Query/athlete_training_result.js
@@ -1,7 +1,20 @@
 const db = require('../../../config/db')
 
 module.exports = {
-    async athletes_trainings_results(_, args){
+    async athletes_trainings_results(_, args) {
         return db('athletes_training_results')
+    },
+    async athlete_training_result(_, { filter }) {
+        if (!filter) return null
+
+        const { id } = filter
+
+        if (id) {
+            return db('athletes_training_results')
+                .where({ id })
+                .first()
+        } else {
+            return null
+        }
     }
 }
