09d30b41e23b4f5c847d74fd31be990c74165c61
Alysson <alysson6801@hotmail.com>
2021-05-28 16:53:41 -0300

    fix: get athlete training result by logged user

--- backend/src/resolvers/Query/athlete_training_result.js
+++ backend/src/resolvers/Query/athlete_training_result.js
@@ -1,17 +1,30 @@
 const db = require('../../../config/db')
 
 module.exports = {
-    async athletes_trainings_results(_, args) {
-        return db('athletes_training_results')
+    async athletes_trainings_results(_, args, ctx) {
+        return db
+            .select(['athletes_training_results.id', 'athletes_training_results.collaborator_athlete_id', 'athletes_training_results.training_id', 'athletes_training_results.result_id', 'athletes_training_results.status'])
+            .table('athletes_training_results')
+            .innerJoin('collaborators', 'collaborators.id', 'athletes_training_results.collaborator_athlete_id')
+            .innerJoin('collaborators_teams', 'collaborators_teams.collaborator_id', 'collaborators.id')
+            .innerJoin('teams', 'collaborators_teams.team_id', 'teams.id')
+            .innerJoin('team_category', 'team_category.id', 'teams.category_id')
+            .whereRaw(`team_category.user_id = ${ctx.user.id}`)
     },
-    async athlete_training_result(_, { filter }) {
+    async athlete_training_result(_, { filter }, ctx) {
         if (!filter) return null
 
         const { id } = filter
 
         if (id) {
-            return db('athletes_training_results')
-                .where({ id })
+            return db
+                .select(['athletes_training_results.id', 'athletes_training_results.collaborator_athlete_id', 'athletes_training_results.training_id', 'athletes_training_results.result_id', 'athletes_training_results.status'])
+                .table('athletes_training_results')
+                .innerJoin('collaborators', 'collaborators.id', 'athletes_training_results.collaborator_athlete_id')
+                .innerJoin('collaborators_teams', 'collaborators_teams.collaborator_id', 'collaborators.id')
+                .innerJoin('teams', 'collaborators_teams.team_id', 'teams.id')
+                .innerJoin('team_category', 'team_category.id', 'teams.category_id')
+                .whereRaw(`athletes_training_results.id = ${id} AND team_category.user_id = ${ctx.user.id}`)
                 .first()
         } else {
             return null
