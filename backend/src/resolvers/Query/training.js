d2ad1f5fc18108e53babcb1713d42d9b87d8a245
Alysson <alysson6801@hotmail.com>
2021-05-28 14:16:11 -0300

    chore: result by mounth and year

--- backend/src/resolvers/Query/training.js
+++ backend/src/resolvers/Query/training.js
@@ -15,5 +15,19 @@ module.exports = {
         } else {
             return null
         }
+    },
+    async resultsMouthYear(_, { filter }) {
+        if (!filter) return null
+
+        const { month, year } = filter
+        if (month) {
+            return db('results')
+                .whereRaw(`MONTH(dt_result) = ${month}`)
+        } else if (year) {
+            return db('results')
+                .whereRaw(`YEAR(dt_result) = ${year}`)
+        } else {
+            return null
+        }
     }
 }
