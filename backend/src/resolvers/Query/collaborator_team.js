525645f4eed51ae2090338b72db0d115614c6dec
Alysson <alysson6801@hotmail.com>
2021-05-28 17:16:59 -0300

    fix:get collaborator team by logged user

--- backend/src/resolvers/Query/collaborator_team.js
+++ backend/src/resolvers/Query/collaborator_team.js
@@ -1,18 +1,27 @@
 const db = require('../../../config/db')
 
 module.exports = {
-    async collaborators_teams(_, args) {
+    async collaborators_teams(_, args, ctx) {
         return db
-            .select()
+            .select(['collaborators_teams.id', 'collaborators_teams.collaborator_id', 'collaborators_teams.team_id', 'collaborators_teams.function', 'collaborators_teams.status', 'user_id'])
             .table('collaborators_teams')
+            .innerJoin('collaborators', 'collaborators.id', 'collaborators_teams.collaborator_id')
+            .innerJoin('teams', 'collaborators_teams.team_id', 'teams.id')
+            .innerJoin('team_category', 'team_category.id', 'teams.category_id')
+            .whereRaw(`team_category.user_id = ${ctx.user.id}`)
     },
-    async collaborator_team(_, { filter }) {
+    async collaborator_team(_, { filter }, ctx) {
         if (!filter) return null
 
         const { id } = filter
         if (id) {
             return db('collaborators_teams')
-                .where({ id })
+                .select(['collaborators_teams.id', 'collaborators_teams.collaborator_id', 'collaborators_teams.team_id', 'collaborators_teams.function', 'collaborators_teams.status', 'user_id'])
+                .table('collaborators_teams')
+                .innerJoin('collaborators', 'collaborators.id', 'collaborators_teams.collaborator_id')
+                .innerJoin('teams', 'collaborators_teams.team_id', 'teams.id')
+                .innerJoin('team_category', 'team_category.id', 'teams.category_id')
+                .whereRaw(`collaborators_teams.id = ${id} AND team_category.user_id = ${ctx.user.id}`)
                 .first()
         } else {
             return null
