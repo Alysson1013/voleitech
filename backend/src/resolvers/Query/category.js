6e861f37c0d79f8eec9e4481f5b7b9c1ea8d891e
Alysson <alysson6801@hotmail.com>
2021-05-28 17:29:55 -0300

    fix:get categories by logged user

--- backend/src/resolvers/Query/category.js
+++ backend/src/resolvers/Query/category.js
@@ -1,23 +1,24 @@
 const db = require('../../../config/db')
 
 module.exports = {
-    async categories(_, args){
+    async categories(_, args, ctx) {
         return db('team_category')
+            .whereRaw(`team_category.user_id = ${ctx.user.id}`)
     },
-    async category(_, { filter }){
+    async category(_, { filter }) {
         if (!filter) return null
 
         const { id, name_category, user_id } = filter
 
-        if (id){
+        if (id) {
             return db('team_category')
-                .where({ id })
+                .whereRaw(`team_category.user_id = ${ctx.user.id}`)
         } else if (name_category) {
             return db('team_category')
-                .where({ name_category })
+                .whereRaw(`team_category.user_id = ${ctx.user.id}`)
         } else if (user_id) {
             return db('team_category')
-                .where({ user_id })
+                .whereRaw(`team_category.user_id = ${ctx.user.id}`)
         } else {
             return null
         }
