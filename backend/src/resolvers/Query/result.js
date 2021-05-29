7c380630e305ca1672cfc78caf9f4795b97fbeaa
Alysson <alysson6801@hotmail.com>
2021-05-29 17:31:34 -0300

    fix: get results by date

--- backend/src/resolvers/Query/result.js
+++ backend/src/resolvers/Query/result.js
@@ -3,25 +3,26 @@ const db = require('../../../config/db')
 module.exports = {
     async results(_, args, ctx) {
         return db
-            .select(['results.id as id', 'dt_result', 'general_points', 'n_floating_serve', 'n_floating_serve_points', 'n_floating_serve_mistake', 'n_trip_serve', 'n_trip_serve_points', 'n_trip_serve_mistake', 'n_attack', 'n_attack_points', 'n_attack_mistake', 'n_dripping_point', 'n_box_point', 'n_block', 'n_block_mistake', 'n_block_used_mistake', 'n_block_points', 'n_general_passes', 'n_pass_mistake', 'n_pass_a', 'n_pass_b', 'n_pass_c', 'n_defense_general', 'n_defense_mistake', 'n_defense_a', 'n_defense_b', 'n_defense_c', 'n_lifting', 'n_lifting_mistake', 'n_lifting_correct', 'n_initiative', 'n_initiative_lack', 'user_id'])
-            .distinct('results.id')
-            .orderBy('results.id', 'asc')
+            .select(['results.id as id', 'training.dt_training as dt_result', 'general_points', 'n_floating_serve', 'n_floating_serve_points', 'n_floating_serve_mistake', 'n_trip_serve', 'n_trip_serve_points', 'n_trip_serve_mistake', 'n_attack', 'n_attack_points', 'n_attack_mistake', 'n_dripping_point', 'n_box_point', 'n_block', 'n_block_mistake', 'n_block_used_mistake', 'n_block_points', 'n_general_passes', 'n_pass_mistake', 'n_pass_a', 'n_pass_b', 'n_pass_c', 'n_defense_general', 'n_defense_mistake', 'n_defense_a', 'n_defense_b', 'n_defense_c', 'n_lifting', 'n_lifting_mistake', 'n_lifting_correct', 'n_initiative', 'n_initiative_lack', 'user_id'])
             .table('results')
+            .distinct('training.dt_training')
+            .orderBy('results.id', 'asc')
             .innerJoin('athletes_training_results', 'athletes_training_results.result_id', 'results.id')
             .innerJoin('collaborators', 'collaborators.id', 'athletes_training_results.collaborator_athlete_id')
             .innerJoin('collaborators_teams', 'collaborators_teams.collaborator_id', 'collaborators.id')
             .innerJoin('teams', 'collaborators_teams.team_id', 'teams.id')
+            .innerJoin('training', 'training.team_id', 'teams.id')
             .innerJoin('team_category', 'team_category.id', 'teams.category_id')
             .whereRaw(`team_category.user_id = ${ctx.user.id}`)
     },
     async result(_, { filter }, ctx) {
         if (!filter) return null
 
-        const { id } = filter
+        const { id, dt_result } = filter
 
         if (id) {
             return db
-                .select(['results.id as id', 'dt_result', 'general_points', 'n_floating_serve', 'n_floating_serve_points', 'n_floating_serve_mistake', 'n_trip_serve', 'n_trip_serve_points', 'n_trip_serve_mistake', 'n_attack', 'n_attack_points', 'n_attack_mistake', 'n_dripping_point', 'n_box_point', 'n_block', 'n_block_mistake', 'n_block_used_mistake', 'n_block_points', 'n_general_passes', 'n_pass_mistake', 'n_pass_a', 'n_pass_b', 'n_pass_c', 'n_defense_general', 'n_defense_mistake', 'n_defense_a', 'n_defense_b', 'n_defense_c', 'n_lifting', 'n_lifting_mistake', 'n_lifting_correct', 'n_initiative', 'n_initiative_lack', 'user_id'])
+                .select(['results.id as id', 'training.dt_training as dt_result', 'general_points', 'n_floating_serve', 'n_floating_serve_points', 'n_floating_serve_mistake', 'n_trip_serve', 'n_trip_serve_points', 'n_trip_serve_mistake', 'n_attack', 'n_attack_points', 'n_attack_mistake', 'n_dripping_point', 'n_box_point', 'n_block', 'n_block_mistake', 'n_block_used_mistake', 'n_block_points', 'n_general_passes', 'n_pass_mistake', 'n_pass_a', 'n_pass_b', 'n_pass_c', 'n_defense_general', 'n_defense_mistake', 'n_defense_a', 'n_defense_b', 'n_defense_c', 'n_lifting', 'n_lifting_mistake', 'n_lifting_correct', 'n_initiative', 'n_initiative_lack', 'user_id'])
                 .table('results')
                 .distinct('results.id')
                 .orderBy('results.id', 'asc')
@@ -30,9 +31,22 @@ module.exports = {
                 .innerJoin('collaborators_teams', 'collaborators_teams.collaborator_id', 'collaborators.id')
                 .innerJoin('teams', 'collaborators_teams.team_id', 'teams.id')
                 .innerJoin('team_category', 'team_category.id', 'teams.category_id')
-                .innerJoin('training')
+                .innerJoin('training', 'training.team_id', 'teams.id')
                 .whereRaw(`results.id = ${id} AND team_category.user_id = ${ctx.user.id}`)
                 .limit(1)
+        } else if (dt_result) {
+            return db
+                .select(['results.id as id', 'training.dt_training as dt_result', 'general_points', 'n_floating_serve', 'n_floating_serve_points', 'n_floating_serve_mistake', 'n_trip_serve', 'n_trip_serve_points', 'n_trip_serve_mistake', 'n_attack', 'n_attack_points', 'n_attack_mistake', 'n_dripping_point', 'n_box_point', 'n_block', 'n_block_mistake', 'n_block_used_mistake', 'n_block_points', 'n_general_passes', 'n_pass_mistake', 'n_pass_a', 'n_pass_b', 'n_pass_c', 'n_defense_general', 'n_defense_mistake', 'n_defense_a', 'n_defense_b', 'n_defense_c', 'n_lifting', 'n_lifting_mistake', 'n_lifting_correct', 'n_initiative', 'n_initiative_lack', 'user_id'])
+                .table('results')
+                .distinct('results.id')
+                .orderBy('results.id', 'asc')
+                .innerJoin('athletes_training_results', 'athletes_training_results.result_id', 'results.id')
+                .innerJoin('collaborators', 'collaborators.id', 'athletes_training_results.collaborator_athlete_id')
+                .innerJoin('collaborators_teams', 'collaborators_teams.collaborator_id', 'collaborators.id')
+                .innerJoin('teams', 'collaborators_teams.team_id', 'teams.id')
+                .innerJoin('team_category', 'team_category.id', 'teams.category_id')
+                .innerJoin('training', 'training.team_id', 'teams.id')
+                .whereRaw(`training.dt_training = '${dt_result}' AND team_category.user_id = ${ctx.user.id}`)
         } else {
             return null
         }
--- backend/src/resolvers/Query/scout.js
+++ backend/src/resolvers/Query/scout.js
@@ -11,19 +11,29 @@ module.exports = {
             .rightJoin('teams', 'teams.id', 'training.team_id')
             .leftJoin('team_category', 'team_category.id', 'teams.category_id')
             .whereRaw(`team_category.user_id = ${ctx.user.id}`)
-        },
-    async scout(_, { filter }) {
+    },
+    async scout(_, { filter }, ctx) {
         if (!filter) return null
 
         const { id, name } = filter
 
         if (id) {
-            return db('scouts')
-                .where({ id })
+            return db
+                .select(['scouts.id as id', 'scouts.name as name', 'v_floating_serve', 'v_floating_serve_points', 'v_floating_serve_mistake', 'v_trip_serve', 'v_trip_serve_points', 'v_trip_serve_mistake', 'v_attack', 'v_attack_points', 'v_attack_mistake', 'v_dripping_point', 'v_box_point', 'v_block', 'v_block_mistake', 'v_block_used_mistake', 'v_block_points', 'v_general_passes', 'v_pass_mistake', 'v_pass_a', 'v_pass_b', 'v_pass_c', 'v_defense_general', 'v_defense_mistake', 'v_defense_a', 'v_defense_b', 'v_defense_c', 'v_lifting', 'v_lifting_mistake', 'v_lifting_correct', 'v_initiative', 'v_initiative_lack'])
+                .table('scouts')
+                .leftJoin('training', 'training.scout_id', 'scouts.id')
+                .rightJoin('teams', 'teams.id', 'training.team_id')
+                .leftJoin('team_category', 'team_category.id', 'teams.category_id')
+                .whereRaw(`scouts.id = ${id} AND team_category.user_id = ${ctx.user.id}`)
                 .first()
         } else if (name) {
-            return db('scouts')
-                .where({ name })
+            return db
+                .select(['scouts.id as id', 'scouts.name as name', 'v_floating_serve', 'v_floating_serve_points', 'v_floating_serve_mistake', 'v_trip_serve', 'v_trip_serve_points', 'v_trip_serve_mistake', 'v_attack', 'v_attack_points', 'v_attack_mistake', 'v_dripping_point', 'v_box_point', 'v_block', 'v_block_mistake', 'v_block_used_mistake', 'v_block_points', 'v_general_passes', 'v_pass_mistake', 'v_pass_a', 'v_pass_b', 'v_pass_c', 'v_defense_general', 'v_defense_mistake', 'v_defense_a', 'v_defense_b', 'v_defense_c', 'v_lifting', 'v_lifting_mistake', 'v_lifting_correct', 'v_initiative', 'v_initiative_lack'])
+                .table('scouts')
+                .leftJoin('training', 'training.scout_id', 'scouts.id')
+                .rightJoin('teams', 'teams.id', 'training.team_id')
+                .leftJoin('team_category', 'team_category.id', 'teams.category_id')
+                .whereRaw(`scouts.name = '${name}' AND team_category.user_id = ${ctx.user.id}`)
                 .first()
         } else {
             return null
