f8a4f0340f139ec9b0d41686fae569324bf02dc1
Alysson <alysson6801@hotmail.com>
2021-05-29 19:15:34 -0300

    fix: training types query

--- backend/src/resolvers/Query/training_type.js
+++ backend/src/resolvers/Query/training_type.js
@@ -1,21 +1,40 @@
 const db = require('../../../config/db')
 
 module.exports = {
-    async trainings_types(_, args) {
-        return db('training_type')
+    async trainings_types(_, args, ctx) {
+        return db
+            .select(['training_type.id as id', 'training_type_name', 'training_type.describe', 'team_category.user_id'])
+            .table('training_type')
+            .distinct('training_type.id')
+            .innerJoin('training', 'training.training_type_id', 'training_type.id')
+            .innerJoin('teams', 'teams.id', 'training.team_id')
+            .leftJoin('team_category', 'team_category.id', 'teams.category_id')
+            
     },
-    async training_type(_, { filter }) {
+    async training_type(_, { filter }, ctx) {
         if (!filter) return null
 
         const { id, training_type_name } = filter
 
         if (id) {
-            return db('training_type')
-                .where({ id })
+            return db
+                .select(['training_type.id as id', 'training_type_name', 'training_type.describe', 'team_category.user_id'])
+                .table('training_type')
+                .distinct('training_type.id')
+                .innerJoin('training', 'training.training_type_id', 'training_type.id')
+                .innerJoin('teams', 'teams.id', 'training.team_id')
+                .leftJoin('team_category', 'team_category.id', 'teams.category_id')
+                .whereRaw(`training_type.id = ${id} AND training_type.id AND team_category.user_id = ${ctx.user.id}`)
                 .first()
         } else if (training_type_name) {
-            return db('training_type')
-                .where({ training_type_name })
+            return db
+                .select(['training_type.id as id', 'training_type_name', 'training_type.describe', 'team_category.user_id'])
+                .table('training_type')
+                .distinct('training_type.id')
+                .innerJoin('training', 'training.training_type_id', 'training_type.id')
+                .innerJoin('teams', 'teams.id', 'training.team_id')
+                .leftJoin('team_category', 'team_category.id', 'teams.category_id')
+                .whereRaw(`training_type.training_type_name = '${training_type_name}' AND training_type.id AND team_category.user_id = ${ctx.user.id}`)
                 .first()
         } else {
             return null
