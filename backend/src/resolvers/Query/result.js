213f19229b35c4842fcafc23a4f409b6e497040f
Alysson <alysson6801@hotmail.com>
2021-05-29 18:00:28 -0300

    fix: collaborator results in collaborator query

--- backend/src/resolvers/Query/result.js
+++ backend/src/resolvers/Query/result.js
@@ -5,7 +5,7 @@ module.exports = {
         return db
             .select(['results.id as id', 'training.dt_training as dt_result', 'general_points', 'n_floating_serve', 'n_floating_serve_points', 'n_floating_serve_mistake', 'n_trip_serve', 'n_trip_serve_points', 'n_trip_serve_mistake', 'n_attack', 'n_attack_points', 'n_attack_mistake', 'n_dripping_point', 'n_box_point', 'n_block', 'n_block_mistake', 'n_block_used_mistake', 'n_block_points', 'n_general_passes', 'n_pass_mistake', 'n_pass_a', 'n_pass_b', 'n_pass_c', 'n_defense_general', 'n_defense_mistake', 'n_defense_a', 'n_defense_b', 'n_defense_c', 'n_lifting', 'n_lifting_mistake', 'n_lifting_correct', 'n_initiative', 'n_initiative_lack', 'user_id'])
             .table('results')
-            .distinct('training.dt_training')
+            .distinct('results.id')
             .orderBy('results.id', 'asc')
             .innerJoin('athletes_training_results', 'athletes_training_results.result_id', 'results.id')
             .innerJoin('collaborators', 'collaborators.id', 'athletes_training_results.collaborator_athlete_id')
@@ -30,8 +30,8 @@ module.exports = {
                 .innerJoin('collaborators', 'collaborators.id', 'athletes_training_results.collaborator_athlete_id')
                 .innerJoin('collaborators_teams', 'collaborators_teams.collaborator_id', 'collaborators.id')
                 .innerJoin('teams', 'collaborators_teams.team_id', 'teams.id')
-                .innerJoin('team_category', 'team_category.id', 'teams.category_id')
                 .innerJoin('training', 'training.team_id', 'teams.id')
+                .innerJoin('team_category', 'team_category.id', 'teams.category_id')
                 .whereRaw(`results.id = ${id} AND team_category.user_id = ${ctx.user.id}`)
                 .limit(1)
         } else if (dt_result) {
