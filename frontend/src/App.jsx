0ccbe8ec17feaf6472a7b49e73a7a38447867801
Alysson <alysson6801@hotmail.com>
2021-06-08 18:49:09 -0300

    chore: input component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header'
 import 'animate.css';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import LoginForm from './LoginForm'
+import LoginCadastro from './LoginCadastro' 
+
+const Login = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<LoginForm />} />
+                <Route path="/cadastro" element={<LoginCadastro />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Login
