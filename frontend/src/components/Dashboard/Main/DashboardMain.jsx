aa6172aed9bc13af8c31850006e50253ef2e3415
Alysson <alysson6801@hotmail.com>
2021-08-03 12:50:58 -0300

    chore: add content component

--- frontend/src/components/Dashboard/Main/DashboardMain.jsx
+++ frontend/src/components/Dashboard/Main/DashboardMain.jsx
@@ -3,13 +3,40 @@ import styles from './DashboardMain.module.css'
 import Options from '../Options/Options'
 import { Container, Row, Col } from 'react-bootstrap'
 
+const teste = [
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1",
+  "teste1"
+]
+
 function DashboardMain() {
   return (
     <Container className={`animate__animated animate__bounce animate__fadeInDown ${styles.dash}`}>
       <Row>
         <Options />
-        <Col>
-          Teste
+        <Col className={`${styles.centerCol}`}>
+            {
+              teste.map((value, index) => (
+                <h1 key={index}>
+                  {value}
+                </h1>
+              ))
+            }
         </Col>
       </Row>
     </Container>
