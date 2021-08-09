8dae9e2ccd6e85acedc97b0cb1010ca92c49676a
Alysson <alysson6801@hotmail.com>
2021-08-09 11:09:57 -0300

    chore: get athletes and assistants

--- backend/src/resolvers/Query/collaborator.js
+++ backend/src/resolvers/Query/collaborator.js
@@ -28,5 +28,25 @@ module.exports = {
         } else {
             return null
         }
+    },
+    async athletes(_, args, ctx){
+        return db
+            .select(['collaborators.id', 'collaborators.phone_1', 'collaborators.phone_2', 'collaborators.phone_3', 'collaborators.email_1', 'collaborators.email_2', 'collaborators.email_3', 'collaborators.name', 'collaborators.dt_birth', 'collaborators.function AS function', 'collaborators.n_enrollment_atl', 'collaborators.n_enrollment_ast', 'collaborators.positions', 'collaborators.n_uniform', 'collaborators.height', 'collaborators.weight', 'collaborators.width', 'collaborators.gender', 'collaborators.bmi', 'collaborators.jump_distance', 'collaborators.jump_height', 'collaborators.describe', 'team_category.user_id '])
+            .distinct('collaborators.id')
+            .table("collaborators")
+            .innerJoin("collaborators_teams", "collaborators_teams.collaborator_id", "collaborators.id")
+            .innerJoin("teams", "teams.id", "collaborators_teams.team_id")
+            .innerJoin("team_category", "team_category.id", "teams.category_id")
+            .whereRaw(`team_category.user_id = ${ctx.user.id} AND collaborators.function = 'athlete' OR collaborators.function = 'both'`)
+    },
+    async assistants(_, args, ctx){
+        return db
+            .select(['collaborators.id', 'collaborators.phone_1', 'collaborators.phone_2', 'collaborators.phone_3', 'collaborators.email_1', 'collaborators.email_2', 'collaborators.email_3', 'collaborators.name', 'collaborators.dt_birth', 'collaborators.function AS function', 'collaborators.n_enrollment_atl', 'collaborators.n_enrollment_ast', 'collaborators.positions', 'collaborators.n_uniform', 'collaborators.height', 'collaborators.weight', 'collaborators.width', 'collaborators.gender', 'collaborators.bmi', 'collaborators.jump_distance', 'collaborators.jump_height', 'collaborators.describe', 'team_category.user_id '])
+            .distinct('collaborators.id')
+            .table("collaborators")
+            .innerJoin("collaborators_teams", "collaborators_teams.collaborator_id", "collaborators.id")
+            .innerJoin("teams", "teams.id", "collaborators_teams.team_id")
+            .innerJoin("team_category", "team_category.id", "teams.category_id")
+            .whereRaw(`team_category.user_id = ${ctx.user.id} AND collaborators.function = 'assistant' OR collaborators.function = 'both'`)
     }
 }
